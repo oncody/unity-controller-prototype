fa7602098788fbd86d6b038f944dede9241b16ef
Cody Smith <37386486+oncody@users.noreply.github.com>
2023-02-21 22:41:12 -0700

    isgrounded still is not working

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,15 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float distance = 0.4f;
+    public LayerMask layerMask;
     public CharacterController controller;
 
     public float speed = 12f;
 
     public float gravity = -9.81f;
     private Vector3 velocity;
+
+    private bool isGrounded;
     
     // Start is called before the first frame update
     void Start()
@@ -20,6 +25,29 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Get the collider component
+        Collider collider = GetComponent<Collider>();
+
+        // Get the top position of the object
+        float topPosition = transform.position.y + collider.bounds.extents.y;
+
+        // Get the bottom position of the object
+        float bottomPosition = transform.position.y - collider.bounds.extents.y;
+
+
+        Debug.Log($"topPosition: {topPosition}");
+        Debug.Log($"bottomPosition: {bottomPosition}");
+        
+        RaycastHit hit;
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, out hit, distance, layerMask);
+        
+        Debug.Log($"isGrounded: {isGrounded}");
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -28,5 +56,14 @@ public class PlayerMovement : MonoBehaviour
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+    }
+    
+    public bool IsGrounded()
+    {
+        return isGrounded;
     }
 }
