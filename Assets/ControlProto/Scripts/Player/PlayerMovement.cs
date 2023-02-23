54c296e2245f6024d180eb2d569e96b512c13ae4
Cody Smith <37386486+oncody@users.noreply.github.com>
2023-02-22 18:40:00 -0700

    removing unused code and simplifying

--- Assets/ControlProto/Scripts/Player/PlayerMovement.cs
+++ Assets/ControlProto/Scripts/Player/PlayerMovement.cs
@@ -18,34 +18,13 @@ namespace ControlProto.Scripts.Player {
         void Update() {
             Transform currentTransform = transform;
 
-            // Store the current position and time for the next frame
-            Vector3 currentPosition = currentTransform.position;
-
-            // Get the collider component
-            // TODO: CODY change this to bind to a collider
-            Collider currentCollider = GetComponent<Collider>();
-            Bounds bounds = currentCollider.bounds;
-
-            float topPosition = currentPosition.y + bounds.extents.y;
-            float bottomPosition = currentPosition.y - bounds.extents.y;
-            float characterHeight = topPosition - bottomPosition;
-
-            // Debug.Log($"topPosition: {topPosition}");
-            // Debug.Log($"bottomPosition: {bottomPosition}");
-            // Debug.Log($"characterHeight: {characterHeight}");
-
-            RaycastHit hit;
-
             // Set the distance of the ray to be slightly larger than the character's height
-            float rayDistance = (characterHeight / 2.0f) + Maths.PositiveValue(globals.GroundCheckDistance);
-            Debug.Log($"rayDistance: {rayDistance}");
-
-            isGrounded = Physics.SphereCast(currentPosition, characterController.radius, Vector3.down, out hit, rayDistance, Physics.AllLayers, QueryTriggerInteraction.Ignore);
-
-            Debug.Log($"isGrounded: {isGrounded}");
+            float rayDistance = (characterController.height / 2.0f) + Maths.PositiveValue(globals.GroundCheckDistance);
+            isGrounded = Physics.SphereCast(currentTransform.position, characterController.radius, Vector3.down, out _, rayDistance, Physics.AllLayers, QueryTriggerInteraction.Ignore);
+            // Debug.Log($"isGrounded: {isGrounded}");
 
             if (isGrounded && velocity.y < 0) {
-                velocity.y = 0;
+                velocity.y = -2;
             }
 
             float horizontalMovementValue = controllerHandler.HorizontalMovementInput();
@@ -54,12 +33,8 @@ namespace ControlProto.Scripts.Player {
             Vector3 directionToMoveTo = currentTransform.right * horizontalMovementValue + currentTransform.forward * verticalMovementValue;
             characterController.Move(directionToMoveTo * (Maths.PositiveValue(globals.DefaultMovementSpeed) * Time.deltaTime));
 
-            velocity.y -= globals.Gravity * Time.deltaTime;
+            velocity.y -= Maths.PositiveValue(globals.Gravity) * Time.deltaTime;
             characterController.Move(velocity * Time.deltaTime);
-        }
-
-        public bool IsGrounded() {
-            return isGrounded;
         }
     }
 }
